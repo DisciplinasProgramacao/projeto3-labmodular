8af2bb1f70d426f29b328e44db09cf08bdd5b00d
Pedro Farias Pires <125492933+pedrinhofp10@users.noreply.github.com>
2023-10-11 16:44:41 -0300

    Update Vaga.java

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -5,7 +5,46 @@ public class Vaga {
 	private String id;
 	private boolean disponivel;
 
-	public Vaga(int fila, int numero) {
+	public Vaga(int fila, int numero) {import java.util.Date;
+
+public class UsoDeVaga {
+    private String identificadorCliente;
+    private int numeroVaga;
+    private Date dataInicio;
+    private Date dataFim;
+    private double valorPago;
+
+    public UsoDeVaga(String identificadorCliente, int numeroVaga, Date dataInicio) {
+        this.identificadorCliente = identificadorCliente;
+        this.numeroVaga = numeroVaga;
+        this.dataInicio = dataInicio;
+    }
+
+    public void encerrarUso(Date dataFim, double valorPago) {
+        this.dataFim = dataFim;
+        this.valorPago = valorPago;
+    }
+
+    public String getIdentificadorCliente() {
+        return identificadorCliente;
+    }
+
+    public int getNumeroVaga() {
+        return numeroVaga;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+}
 		
 	}
 
